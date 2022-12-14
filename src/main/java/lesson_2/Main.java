a2f95aae7dd5dee5fd06673805f0d06f9d36a287
Maxim <ozmaxim@gmail.com>
2022-12-14 21:41:43 +0300

    Домашнее задание к уроку 2

---
+++ src/main/java/lesson_2/Main.java
@@ -0,0 +1,14 @@
+package lesson_2;
+
+public class Main {
+    public static void main(String args[]) {
+        int[] arr = {12, 11, 13, 5, 6, 7, 89, 56, 125, 21, 41};
+        int n = arr.length;
+
+        HeapSort heapSort = new HeapSort();
+        heapSort.sort(arr);
+
+        System.out.println("Sorted array is");
+        heapSort.printArray(arr);
+    }
+}
